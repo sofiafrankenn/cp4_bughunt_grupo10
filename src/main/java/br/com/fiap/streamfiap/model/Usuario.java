42f06a40535ca0f0b8b34576900b78d3ddf12445
Sofia Frankenn <sofiafrankenn@gmail.com>
2026-09-04 09:22:32 -0300

    fix: bug02 - corrige atribuicao do nome no construtor de Usuario

--- src/main/java/br/com/fiap/streamfiap/model/Usuario.java
+++ src/main/java/br/com/fiap/streamfiap/model/Usuario.java
@@ -20,7 +20,7 @@ public class Usuario {
     }
 
     public Usuario(String nome, int idade, double creditos) {
-        nome = nome;
+        this.nome = nome;
         this.idade = idade;
         this.creditos = creditos;
     }
